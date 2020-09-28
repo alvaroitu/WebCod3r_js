26b607f940a83d65786ad674d1173eca00166b71
Alvaro Itu <contato@alvaroitu.com>
2020-09-28 16:06:59 -0300

    iniciando repositorio

---
+++ Aulas/Fundamentos/funcao2.js
@@ -0,0 +1,15 @@
+const imprimirSoma = function(a,b){
+    console.log(a + b);
+}
+
+imprimirSoma(2,3);
+
+const soma = (a, b) => {
+    return a + b;
+}
+
+console.log(soma(3,3));
+
+const maissoma = (a, b) => a + b;
+
+console.log(maissoma(4,4));
---
+++ Aulas/Fundamentos/obejto.js
@@ -0,0 +1,13 @@
+const prod = {};
+
+prod.nome = 'celular';
+prod.numero = '55555';
+
+console.log(prod);
+
+const prod1 = {
+    nome: 'camisa',
+    preco: 80.00,
+}
+
+console.log(prod1);
